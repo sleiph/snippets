fb1cfd190e13b3392df333e30e1742d6f8e92e38
Ricardo Gomes <sleiph@gmail.com>
2020-09-05 14:35:04 -0300

    deixando mais legivel

--- primoRapido.cs
+++ primoRapido.cs
@@ -5,14 +5,14 @@ class primos {
         int raiz = (int)Math.Sqrt((double)caso);
         if (caso <= 1)
             return false;
-        else {
-            if (caso != 2 && caso % 2 == 0)
-                return false;
-            else
-                for (int i = 3; i <= raiz; i += 2)
-                    if (caso % i == 0)
-                        return false;
-        }
+        else if (caso == 2)
+            return true;
+        else if (caso % 2 == 0)
+            return false;
+        else
+            for (int i = 3; i <= raiz; i += 2)
+                if (caso % i == 0)
+                    return false;
         return true;
     }
 }
