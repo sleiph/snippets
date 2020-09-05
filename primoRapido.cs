b2d317448b58a98a9fb7dc91d56525b58c6cd7ed
Ricardo Gomes <sleiph@gmail.com>
2020-09-05 14:15:18 -0300

    transformando em funcao

--- primoRapido.cs
+++ primoRapido.cs
@@ -1,20 +1,18 @@
 using System; 
 
 class primos {
-    static void Main(string[] args) { 
-        Console.Write("Digite o número que você quer saber se é primo: \n");
-        int caso = Int32.Parse(System.Console.ReadLine()), raiz = (int)Math.Sqrt((double)caso);;
-        bool primo = true;
-        if (caso != 2 && caso % 2 == 0) primo = false;
-        else{
-            for (int i = 3; i <= raiz; i += 2){
-                if (caso % i == 0){
-                    primo = false;
-                    break;
-                }
-            }
+    static bool Primo(int caso) {
+        int raiz = (int)Math.Sqrt((double)caso);
+        if (caso == 1)
+            return false;
+        else {
+            if (caso != 2 && caso % 2 == 0)
+                return false;
+            else
+                for (int i = 3; i <= raiz; i += 2)
+                    if (caso % i == 0)
+                        return false;
         }
-        if (primo) Console.Write("{0} é primo.\n", caso);
-        else Console.Write("{0} não é primo.\n", caso);
+        return true;
     }
 }
