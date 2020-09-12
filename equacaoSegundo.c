58f8ef423370f731dfb3083f0cd0826a8f840a39
Ricardo Gomes <sleiph@gmail.com>
2020-09-12 18:30:28 -0300

    adicionando um loop na leitura dos valores

--- equacaoSegundo.c
+++ equacaoSegundo.c
@@ -1,31 +1,33 @@
 #include<stdio.h>
 #include<math.h>
 
-int main(int argc, char** argv)
-{
-    float a, b, c, x1, x2, delta;
+int main(int argc, char** argv) {
+  double a, b, c, delta, x1, x2;
     
-    printf("Digite o valor de a:");
-    scanf("%f", &a);
-    printf("Digite o valor de b:");
-    scanf("%f", &b);
-    printf("Digite o valor de c:");
-    scanf("%f", &c);
+  while (1) {
+    printf("Digite o valor de a: ");
+    scanf("%lf", &a);
+    if (a != 0.0) break;
+    printf("O valor de a precisa ser diferente de zero\n");
+  }
+  printf("Digite o valor de b: ");
+  scanf("%lf", &b);
+  printf("Digite o valor de c: ");
+  scanf("%lf", &c);
     
-    if (a == 0) printf("Essa não é uma equação de segundo grau :(");
-    else{
-      delta = (b*b) - (4*a*c);
-      if (delta < 0) printf("A equação não possui raízes reais.");
-      else{
-        x1 = (-b + sqrt(delta)) / (2*a);
-        x2 = (-b - sqrt(delta)) / (2*a);
-        if (x1==x2) printf("O valor de x é %.2f", x1);
-        else{
-          printf("O valor de x1 é %.2f\n", x1);
-          printf("O valor de x2 é %.2f", x2);
-        }
-      }
+  delta = (b * b) - (4 * a * c);
+    
+  if (delta < 0.0)
+    printf("A equação não possui raízes reais.");
+  else {
+    x1 = (-b + sqrt(delta)) / (2*a);
+    x2 = (-b - sqrt(delta)) / (2*a);
+    if (x1 == x2)
+      printf("O valor de x é %.3lf", x1);
+    else {
+      printf("O valor de x1 é %.3lf\n", x1);
+      printf("O valor de x2 é %.3lf", x2);
     }
-    
-    return 0;
+  }
+  return 0;
 }
